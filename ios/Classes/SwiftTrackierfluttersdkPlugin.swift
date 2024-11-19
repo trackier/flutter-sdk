52dff03568aa858ca6d3bbd8374e64aa5057966b
Sanu gupta <sanugupta266@gmail.com>
2024-11-19 12:28:19 +0530

    fix-namespace-issue

--- ios/Classes/SwiftTrackierfluttersdkPlugin.swift
+++ ios/Classes/SwiftTrackierfluttersdkPlugin.swift
@@ -234,7 +234,7 @@ public class SwiftTrackierfluttersdkPlugin: NSObject, FlutterPlugin, DeepLinkLis
 		let deeplinkKey = "\(dict?["deeplinkCallback"] as? String ?? "")"
 		let config = TrackierSDKConfig(appToken: appToken , env: environment)
 		config.setAppSecret(secretId: secretId, secretKey: secretKey)
-		config.setSDKVersion(sdkVersion: "1.6.60")
+		config.setSDKVersion(sdkVersion: "1.6.61")
 		if (!deeplinkKey.isEmpty) {
 			config.setDeeplinkListerner(listener: self)
 		}
